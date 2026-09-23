58c202debd178d40c33a8de7fb85df806940ee7d
coded by urmi <urmilarajapurkar953@gmail.com>
2026-09-23 15:27:31 +0530

    fix: configure Redis connection and enable toggle

--- src/queue/redis/connection.ts
+++ src/queue/redis/connection.ts
@@ -1,28 +1,3 @@
-// import { Redis } from "ioredis";
-// import logger from "../../common/logger";
-
-// const redisUrl = process.env.REDIS_URL;
-
-// export const redisConnection = redisUrl
-//   ? new Redis(redisUrl, {
-//       maxRetriesPerRequest: null,
-//       tls: redisUrl.startsWith("rediss://") ? {} : undefined,
-//     })
-//   : new Redis({
-//       host: process.env.REDIS_HOST || "localhost",
-//       port: Number(process.env.REDIS_PORT) || 6379,
-//       password: process.env.REDIS_PASSWORD || undefined,
-//       maxRetriesPerRequest: null,
-//     });
-
-// redisConnection.on("connect", () => {
-//   logger.info("Redis connected successfully");
-// });
-
-// redisConnection.on("error", (error) => {
-//   logger.error({ error }, "Redis connection error");
-// });
-
 import { Redis, RedisOptions } from "ioredis";
 import logger from "../../common/logger";
 
